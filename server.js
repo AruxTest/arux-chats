e7990284fd06fa5d5cc0d6a987c3e8b930049628
AruxTest <98324685+AruxTest@users.noreply.github.com>
2022-01-24 14:56:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,1 @@
-const WebSocket = require('ws');
-
-const server = new WebSocket.Server({ port: 3000 });
-
-server.on('connection', ws => {
-    ws.on('message', message => {
-        server.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(String(message));
-            }
-        });
-    });
-    
-    ws.send('Добро пожаловать в CodeDojo.');
-});
+heroku buildpacks:set heroku/nodejs
